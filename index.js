193f255d313c8cefb3a7d79b1226013f71f53cd6
Will Gray <WWGray4@gmail.com>
2015-06-30 02:19:58 -0700

    bugfix and todos

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//!todo: Handle correct group broadcasting, close window on joining new group, pictures, any other functionality we want
+
 var url = require('url');
 var express = require('express');
 var group = require('./lib/group');
@@ -28,6 +30,7 @@ io.on('connection', function (socket) {
         //Worth noting this will mess up if two groups are present with the same name.
         var newGroup = group.createGroup(groupId);
         groups[groupId] = newGroup;
+        newGroup.addUser(socket.id);
 
         /*
         userRoutes[newGroup.userUrl] = '^\\/' + newGroup.userUrl + '\\/(\\d+)$';
@@ -41,8 +44,11 @@ io.on('connection', function (socket) {
     });
 
     socket.on('Join Group', function (groupId) {
-        socket.emit('Group Id', groupId);
-        socket.emit('Current Order', groups[groupId].currentOrder)
+        if (groups[groupId]) {
+            groups[groupId].addUser(socket.id);
+            socket.emit('Group Id', groupId);
+            socket.emit('Current Order', groups[groupId].currentOrder)
+        }
     });
 
     /*
@@ -62,7 +68,12 @@ io.on('connection', function (socket) {
         var voteGroup = groups[newVote.groupId];
 
         voteGroup.acceptNewVote(newVote);
+
+        //Figure this out
+        /*for (var member in voteGroup.members) {
+            socket.emit();
+            io.sockets.socket(member).emit('Current Order', groups[newVote.groupId].getCurrentOrder());
+        }*/
         socket.emit('Current Order', groups[newVote.groupId].getCurrentOrder());
-        socket.broadcast.emit('Current Order', groups[newVote.groupId].getCurrentOrder());
     });
 });
