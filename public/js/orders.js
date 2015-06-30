d73035a1b48e4eb75d5274c36f11895c473d3778
Will Gray <WWGray4@gmail.com>
2015-06-29 22:01:42 -0700

    Messing around with create-order.click

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var groupId = 1;
+
     if (window.location.pathname !== "/") { //TODO: better parsing
         //join room id in pathname
     } else {
@@ -7,6 +9,42 @@ $(document).ready(function(){
 
     //Hook up the create-group message
     $('#create-order').click(function () {
-        socket.emit('New Group');
+        $('#order-id').text(groupId);
+        socket.emit('New Group', groupId);
+        groupId++;
     });
+
+    $('#choice-one-vote').click(function () {
+        socket.emit('New Vote', {
+            user : 'hi',
+            vote : 'Plain Cheese'
+        });
+    });
+
+    $('#choice-two-vote').click(function () {
+        socket.emit('New Vote', {
+            user : '',
+            vote : 'Meat Loves'
+        });
+    });
+
+    $('#choice-three-vote').click(function () {
+        socket.emit('New Vote', {
+            user : '',
+            vote : 'Veggie'
+        });
+    });
+
+    $('#choice-four-vote').click(function () {
+        socket.emit('New Vote', {
+            user : '',
+            vote : 'Hawaiian'
+        });
+    });
+
+    socket.on('Group Links', function (links) {
+        $('#order-id').text(links.userUrl);
+    });
+
+
 });
